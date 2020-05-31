3bda530f0d77b4f374a56c2977fce478dd7c788f
caio.alarcon <caio.alarcon@usp.br>
2020-05-30 23:26:48 -0300

    não rode no seu pc os scripts que esse troço cria

--- criascript.c
+++ criascript.c
@@ -15,18 +15,20 @@ int main(){
 	
                         //densidade
         for(k=10;k<=10000;k*=10)               //altura
-            for(l=k;l<=10000;l*=10)
+            for(l=k;l<=100000;l*=10)
                 for(j=0.0;j<0.40;j+=0.05){
                     fprintf(p,"./maze -g %d %d %lf -w arquivo%d.maze --seed %u ",k,l,j,n,(unsigned int)rand());
-                    if(k<100)fprintf(p,"& disown\n");
+                    if(k<1000)fprintf(p,"& disown\n");
                         else fprintf(p,"\n");
                     for(i=0;i<=5;i++) {                       //algoritmo   
                         fprintf(p2,"./maze -s arquivo%d.maze %u --log ",n,i);
-                        if(k<100)fprintf(p2,"& disown\n");
+                        if(k<1000)fprintf(p2,"& disown\n");
                         else fprintf(p2,"\n");
                     }
                 n++;         
             }
+    
+    fprintf(p,"./testatudo.sh \n");
 	fclose (p);
     fclose (p2);
     
